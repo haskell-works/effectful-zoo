1de8548e2407cdea622e90b6655248f44a10c135
John Ky <john.ky@iohk.io>
2025-01-04 10:14:36 +1100

    New property function and PropertyT type

--- components/hedgehog/Effectful/Zoo/Hedgehog/Api/Tasty.hs
+++ components/hedgehog/Effectful/Zoo/Hedgehog/Api/Tasty.hs
@@ -1,13 +1,18 @@
 module Effectful.Zoo.Hedgehog.Api.Tasty
-  ( UnitTest,
+  ( PropertyTest,
+    UnitTest,
+
+    property,
     unitTest,
   ) where
 
 import HaskellWorks.Prelude
-import Hedgehog (TestT)
+import Hedgehog (PropertyT, TestT)
 import Hedgehog qualified as H
 import Test.Tasty (TestName, TestTree)
 import Test.Tasty.Hedgehog (testProperty)
+
+type PropertyTest = PropertyT IO ()
 
 type UnitTest = TestT IO ()
 
@@ -17,3 +22,10 @@ unitTest :: ()
   -> TestTree
 unitTest desc =
   testProperty desc . H.withTests 1 . H.property . H.test
+
+property :: ()
+  => TestName
+  -> PropertyT IO ()
+  -> TestTree
+property desc =
+  testProperty desc . H.property
