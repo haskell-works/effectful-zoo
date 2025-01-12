b786a920f7656e74387e0270f2087b3e986e64c3
John Ky <john.ky@iohk.io>
2025-01-13 00:34:59 +1100

    Export Property, PropertyT, Test and TestT

--- components/hedgehog/Effectful/Zoo/Hedgehog/Api.hs
+++ components/hedgehog/Effectful/Zoo/Hedgehog/Api.hs
@@ -1,5 +1,9 @@
 module Effectful.Zoo.Hedgehog.Api
-  ( module Effectful.Zoo.Hedgehog.Api.Assert,
+  ( Property,
+    PropertyT,
+    Test,
+    TestT,
+    module Effectful.Zoo.Hedgehog.Api.Assert,
     module Effectful.Zoo.Hedgehog.Api.Hedgehog,
     module Effectful.Zoo.Hedgehog.Api.Failure,
     module Effectful.Zoo.Hedgehog.Api.Journal,
@@ -9,3 +13,4 @@ import Effectful.Zoo.Hedgehog.Api.Assert
 import Effectful.Zoo.Hedgehog.Api.Failure
 import Effectful.Zoo.Hedgehog.Api.Hedgehog
 import Effectful.Zoo.Hedgehog.Api.Journal
+import Hedgehog
