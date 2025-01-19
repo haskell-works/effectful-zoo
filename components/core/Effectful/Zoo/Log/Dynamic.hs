7e5a9b18359535901149297a8c2a8a2406e43bad
John Ky <john.ky@iohk.io>
2025-01-20 00:38:21 +1100

    Make the static version of the Log effect the default

--- components/core/Effectful/Zoo/Log/Dynamic.hs
+++ components/core/Effectful/Zoo/Log/Dynamic.hs
@@ -3,33 +3,4 @@ module Effectful.Zoo.Log.Dynamic
     runLog,
   ) where
 
-import Effectful
-import Effectful.Dispatch.Dynamic
-import Effectful.Zoo.Core
-import Effectful.Zoo.Log.Data.LogMessage
-import Effectful.Zoo.Log.Static qualified as S
-import HaskellWorks.Prelude
-
-data Log i :: Effect where
-  Log
-    :: HasCallStack
-    => LogMessage i
-    -> Log i m ()
-
-  Local
-    :: (i -> i)
-    -> m a
-    -> Log i m a
-
-type instance DispatchOf (Log a) = Dynamic
-
-runLog :: forall i a r. ()
-  => r <: IOE
-  => UnliftStrategy
-  -> (CallStack -> LogMessage i -> Eff r ())
-  -> Eff (Log i : r) a
-  -> Eff r a
-runLog s run =
-  reinterpret (S.runLog s run) $ \env -> \case
-    Log m -> S.log m
-    Local f m -> localSeqUnlift env $ \unlift -> S.localLog f (unlift m)
+import Effectful.Zoo.Log.Dynamic.Effect
